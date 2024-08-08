80bd4bcd3384da4da01b16d22d8f71b94004a452
Arthur Shaw <jashaw2002@gmail.com>
2024-08-07 22:21:11 -0400

    Add sequential palettes based on main palette colors

--- R/colors.R
+++ R/colors.R
@@ -99,9 +99,55 @@ wb_palette <- c(
 )
 
 
-# qualitative
+# ==============================================================================
+# Sequential palettes
+# ==============================================================================
 
-# sequential
+# https://projects.susielu.com/viz-palette?colors=[%22#002f54%22,%22#3b577c%22,%22#6b83a6%22,%22#9db2d1%22,%22#d0e3ff%22]&backgroundColor=%22white%22&fontColor=%22black%22&mode=%22normal%22
+wb_dark_blue_pal <- c(
+  "#002f54",
+  "#3b577c",
+  "#6b83a6",
+  "#9db2d1",
+  "#d0e3ff",
+# #002f54,#3a5982,#6a87b3,#9cb8e7,#d3eeff
+# https://projects.susielu.com/viz-palette?colors=[%22#002f54%22,%22#3a5982%22,%22#6a87b3%22,%22#9cb8e7%22,%22#d3eeff%22]&backgroundColor=%22white%22&fontColor=%22black%22&mode=%22normal%22
+)
+
+wb_light_blue_pal <- c(
+# https://projects.susielu.com/viz-palette?colors=[%22#009fda%22,%22#1276ad%22,%22#124f7f%22,%22#082c54%22,%22#000a2b%22]&backgroundColor=%22white%22&fontColor=%22black%22&mode=%22normal%22
+'#001944', '#004274', '#006fa5', '#069fd8', '#62cfff'
+)
+
+# https://projects.susielu.com/viz-palette?colors=[%22#2c7767%22,%22#57988b%22,%22#81b9b0%22,%22#acdcd6%22,%22#d9fffc%22]&backgroundColor=%22white%22&fontColor=%22black%22&mode=%22normal%22
+wb_dark_green_pal <- c(
+  "#2c7767",
+  "#57988b",
+  "#81b9b0",
+  "#acdcd6",
+  "#d9fffc",
+# https://projects.susielu.com/viz-palette?colors=[%22#002113%22,%22#034c3e%22,%22#2f7a6a%22,%22#60aa98%22,%22#92dcc9%22]&backgroundColor=%22white%22&fontColor=%22black%22&mode=%22normal%22
+# '#002113', '#034c3e', '#2f7a6a', '#60aa98', '#92dcc9'
+)
+
+wb_light_green_pal <- c(
+# https://projects.susielu.com/viz-palette?colors=[%22#002213%22,%22#004c3e%22,%22#007b6a%22,%22#06ac99%22,%22#5addc9%22]&backgroundColor=%22white%22&fontColor=%22black%22&mode=%22normal%22
+# https://gka.github.io/palettes/#/5|s|00a996|ffffe0,ff005e,93003a|1|1
+'#002213', '#004c3e', '#007b6a', '#06ac99', '#5addc9'
+)
+
+wb_grey_pal <- c(
+# https://projects.susielu.com/viz-palette?colors=[%22#242424%22,%22#4d4d4d%22,%22#7b7b7b%22,%22#acacac%22,%22#e0e0e0%22]&backgroundColor=%22white%22&fontColor=%22black%22&mode=%22normal%22
+# https://gka.github.io/palettes/#/5|s|7b7b7b|ffffe0,ff005e,93003a|1|1
+'#242424', '#4d4d4d', '#7b7b7b', '#acacac', '#e0e0e0'
+)
+
+wb_yellow_pal <- c(
+# https://projects.susielu.com/viz-palette?colors=[%22#3a0000%22,%22#603100%22,%22#905c00%22,%22#c58801%22,%22#fdb714%22]&backgroundColor=%22white%22&fontColor=%22black%22&mode=%22normal%22
+# https://gka.github.io/palettes/#/5|s|fdb714|ffffe0,ff005e,93003a|1|1
+'#3a0000', '#603100', '#905c00', '#c58801', '#fdb714'
+)
+
 # color ramps
 
 # diverging
