48c6c12f827115024a1a7f46397d0599b03897c6
Arthur Shaw <jashaw2002@gmail.com>
2024-08-07 22:18:14 -0400

    Add secondary corporate colors
    - Create named objects
    - Add documentation

--- R/colors.R
+++ R/colors.R
@@ -22,6 +22,35 @@ wb_black <- "#231f20"
 #' @export
 wb_white <- "#ffffff"
 
+
+# ==============================================================================
+# Secondary corporate colors
+# ==============================================================================
+
+# warm
+# .. bright
+wb_warm_br_orange_1 <- "#f05023"
+wb_warm_br_yellow <- "#FDB714"
+wb_warm_br_red <- "#eb1c2d"
+wb_warm_br_orange_2 <- "#f78d28"
+# ... neutral/muted
+wb_warm_neut_red <- "#98242b"
+wb_warm_neut_orange <- "#e36c2c"
+wb_warm_neut_yellow <- "#b98d2d"
+
+# cool
+# ... bright
+wb_cool_br_blue <- "#009da7"
+wb_cool_br_green <- "#00ac51"
+wb_cool_br_purple <- "#882b90"
+wb_cool_br_teal <- "#00a996"
+# ... neutral/muted
+wb_cool_neut_purple <- "#614776"
+wb_cool_neut_blue <- "#006068"
+wb_cool_neut_green <- "#006450"
+
+
+
 # https://projects.susielu.com/viz-palette?colors=[%22#002244%22,%22#009fda%22,%22#2c7767%22,%22#00a996%22,%22#7b7b7b%22,%22#fdb714%22]&backgroundColor=%22white%22&fontColor=%22black%22&mode=%22normal%22
 
 #' @title World Bank dark blue
